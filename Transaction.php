c73e9a80a1ca4dd1c7fa8b516f69d4aa474a091f
etsec-cloud <etienne.secondini@hotmail.fr>
2020-10-18 15:01:15 +0200

    rien

--- Transaction.php
+++ Transaction.php
@@ -3,14 +3,27 @@
 
     class Transaction extends General{
         protected $table = "transaction";
+    /**
+     * Save user
+     *
+     * @param array $param
+     * @return void
+     */
 
 
         public function saved($param, $params){
             $statement = "INSERT INTO transaction (date, montant, valide, moyenPaiement, compte_id)
                             VALUES (:date, :montant, :valide, :moyenPaiement, :compte_id)";
-            $statements = "SELECT * from compte WHERE id". $param['compte_id'];
-            $possible = $params["fond"] - $param["montant"];
 
+            // $param["montant"]= is_numeric($param["montant"]);
+            // $param["date"]= ($param["date"]);
+            // $param["valide"]= is_bool($param["valide"]);
+            // $param["moyenPaiement"]= htmlspecialchars($param["moyenPaiement"]);
+            // $param["compte_id"]= is_integer($param["compte_id"]);
+
+            $statements = "SELECT * from compte WHERE id". $param['compte_id'];
+            $this->db->prepare($statement, 'saved', $param);
+            $possible = $params["fond"] - $param["montant"];
             if($possible > 0){
                 $param["date"]= date('d-m-Y');
                 $this->db->prepare($statement, 'save', $param);
@@ -19,7 +32,5 @@
             else{
                 echo "pas assez de fond";
             }
-            
-            
         }
     }
